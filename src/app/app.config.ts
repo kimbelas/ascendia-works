f9a046f862942a5afa2a2c39d4a5427d82ea9c8f
Matthew Belas <belasmatthewkim@gmail.com>
2025-06-18 18:17:55 +0800

    Removed unnecessary code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { ConfigService } from './service/config.service';
-import { FormState } from './state/form.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    ConfigService,
-    FormState,
   ],
 };
